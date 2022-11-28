1eb7815148d51aaae307171020e6326fea141664
yoeoh <yoeooh@gmail.com>
2022-11-28 04:21:47 +0300

    added search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,27 @@
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const navigate = useNavigate();
+
+  const hanldeSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm.length) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+
   return (
     <Paper
       component='form'
-      onSubmit={() => {}}
+      onSubmit={hanldeSubmit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -16,7 +30,14 @@ const SearchBar = () => {
         marginRight: { sm: 5 },
       }}
     >
-      <input className='search-bar' placeholder='Search...' value='' onChange={() => {}} />
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
       <IconButton type='submit' sx={{ padding: '10px', color: 'red' }}>
         <Search />
       </IconButton>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
+
+import { fetchFromApi } from '../utils/fetchFromApi';
+import { Videos } from '../components';
+
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    setVideos([]);
+
+    const fetchEndpoint = 'search';
+    const fetchOptions = { params: { part: 'snippet', q: searchTerm } };
+
+    fetchFromApi(fetchEndpoint, fetchOptions).then((data) => setVideos(data.items));
+  }, [searchTerm]);
+
+  return (
+    <Box padding={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+        Search results for <span style={{ color: '#f31503' }}>{searchTerm}</span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
